7f4a8822d6081b73aed0c34727b971e1d5f8b6dd
sjg99 <38005341+sjg99@users.noreply.github.com>
2019-02-24 23:08:00 -0500

    Creating Turns V1

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApplication2.Model;
 
 namespace WindowsFormsApplication2
 {
@@ -22,20 +23,122 @@ namespace WindowsFormsApplication2
             bttn_Cliente.Location = new Point(((wd/2)+151),((hg/2)-247));
             bttn_NoCliente.Location = new Point(((wd/2)-387),((hg/2)-247));
             bttn_Preferencial.Location = new Point(((wd/2)-387),((hg/2)+15));
+            label1.Location = new Point(wd/2,hg/2);
         }
-
+        private void FinRes(Turno t)
+        {
+            label1.Text = "Su turno es: " + t.Tipo + t.Numero;
+            label1.Visible = true;
+            bttn_Cliente.Visible = false;
+            bttn_NoCliente.Visible = false;
+            bttn_Preferencial.Visible = false;
+            timer1.Enabled = true;
+        }
         private void bttn_Cliente_Click(object sender, EventArgs e)
         {
-            FormM.vt.UpdateScreen();
+            using (BankTEntities db = new BankTEntities())
+            {
+                Turno Turn = new Turno();
+                try
+                {
+                    var getLastTurn = db.Turnoes.Where(x => x.Tipo == "C").OrderByDescending(x => x.ID).First();
+                    if (Convert.ToInt32(getLastTurn.Numero) < 99)
+                    {
+                        Turn.Numero = (Convert.ToInt32(getLastTurn.Numero) + 1).ToString();
+                    }
+                    else
+                    {
+                        Turn.Numero = "1";
+                    }
+                    Turn.Tipo = "C";
+                    Turn.Modulo = "0";
+                    Turn.IDCliente = "666";
+                    Turn.Estado = "En Espera";
+                }
+                catch
+                {
+                    Turn.Numero = "1";
+                    Turn.Tipo = "C";
+                    Turn.Modulo = "0";
+                    Turn.IDCliente = "666";
+                    Turn.Estado = "En Espera";
+                }
+                var TurnCreation = db.Set<Turno>();
+                TurnCreation.Add(Turn);
+                db.SaveChanges();
+                FinRes(Turn);
+            }                
         }
         private void bttn_NoCliente_Click(object sender, EventArgs e)
         {
-
+            using (BankTEntities db = new BankTEntities())
+            {
+                Turno Turn = new Turno();
+                try
+                {
+                    var getLastTurn = db.Turnoes.Where(x => x.Tipo == "U").OrderByDescending(x => x.ID).First();
+                    if (Convert.ToInt32(getLastTurn.Numero) < 99)
+                    {
+                        Turn.Numero = (Convert.ToInt32(getLastTurn.Numero) + 1).ToString();
+                    }
+                    else
+                    {
+                        Turn.Numero = "1";
+                    }
+                    Turn.Tipo = "U";
+                    Turn.Modulo = "0";
+                    Turn.IDCliente = "666";
+                    Turn.Estado = "En Espera";
+                }
+                catch
+                {
+                    Turn.Numero = "1";
+                    Turn.Tipo = "U";
+                    Turn.Modulo = "0";
+                    Turn.IDCliente = "666";
+                    Turn.Estado = "En Espera";
+                }
+                var TurnCreation = db.Set<Turno>();
+                TurnCreation.Add(Turn);
+                db.SaveChanges();
+                FinRes(Turn);
+            }
         }
 
         private void bttn_Preferencial_Click(object sender, EventArgs e)
         {
-
+            using (BankTEntities db = new BankTEntities())
+            {
+                Turno Turn = new Turno();
+                try
+                {
+                    var getLastTurn = db.Turnoes.Where(x => x.Tipo == "P").OrderByDescending(x => x.ID).First();
+                    if (Convert.ToInt32(getLastTurn.Numero) < 99)
+                    {
+                        Turn.Numero = (Convert.ToInt32(getLastTurn.Numero) + 1).ToString();
+                    }
+                    else
+                    {
+                        Turn.Numero = "1";
+                    }
+                    Turn.Tipo = "P";
+                    Turn.Modulo = "0";
+                    Turn.IDCliente = "666";
+                    Turn.Estado = "En Espera";
+                }
+                catch
+                {
+                    Turn.Numero = "1";
+                    Turn.Tipo = "P";
+                    Turn.Modulo = "0";
+                    Turn.IDCliente = "666";
+                    Turn.Estado = "En Espera";
+                }
+                var TurnCreation = db.Set<Turno>();
+                TurnCreation.Add(Turn);
+                db.SaveChanges();
+                FinRes(Turn);
+            }
         }
 
         private void bttn_Cliente_MouseMove(object sender, MouseEventArgs e)
@@ -71,6 +174,14 @@ namespace WindowsFormsApplication2
         private void Form1_Load(object sender, EventArgs e)
         {
             FormM.vt.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+            timer1.Enabled = false;
+            Dispose(false);
         }
     }
 }
